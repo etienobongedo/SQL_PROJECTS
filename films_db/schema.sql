3de001761697c71ab8e39d58d9c11281b1005317
Etienobong Edo <etienobongedo04@gmail.com>
2025-06-12 17:53:41 +0100

    Update schema.sql

--- films_db/schema.sql
+++ films_db/schema.sql
@@ -1,1 +1,34 @@
+--SCHEMA for the films database
 
+CREATE TABLE films (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(256),
+        release_year INTEGER,
+        country VARCHAR(150),
+        duration INTEGER,
+        language VARCHAR(20),
+        certification VARCHAR(15),
+        gross BIGINT,
+        budget BIGINT
+  );
+CREATE TABLE people (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(150),
+        birthdate DATE,
+        deathdate DATE
+  );
+CREATE TABLE reviews (
+        id SERIAL PRIMARY KEY,
+        film_id INTEGER REFERENCES films(id),
+        num_user INTEGER,
+        num_critic INTEGER,
+        imdb_score REAL,
+        num_votes INTEGER,
+        facebook_likes INTEGER
+   );
+CREATE TABLE roles (
+        id SERIAL PRIMARY KEY,
+        film_id INTEGER REFERENCES films(id),
+        person_id INTEGER REFERENCES people(id),
+        role VARCHAR(15)
+    );
